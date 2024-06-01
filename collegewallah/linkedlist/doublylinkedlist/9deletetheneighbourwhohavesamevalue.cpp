e3dd41cb07c939561bf66d0121f1ecef609f63a2
sharmapravin76 <sharmapravin7631@gmail.com>
2024-06-01 17:21:02 +0530

    changes

--- collegewallah/linkedlist/doublylinkedlist/9deletetheneighbourwhohavesamevalue.cpp
+++ collegewallah/linkedlist/doublylinkedlist/9deletetheneighbourwhohavesamevalue.cpp
@@ -31,53 +31,49 @@ public:
             cout << temp->val << "<->";
             temp = temp->next;
         }
-        cout<< endl;
+        cout << endl;
     }
-    void insertAtStart(int val)
+       void insertAtEnd(int val)
     {
         node *new_node = new node(val);
-        if (head == NULL)
+        if (tail == NULL)
         {
             head = new_node;
             tail = new_node;
             return;
         }
-        new_node->next = head;
-        head->prev = new_node;
-        head = new_node;
+        tail->next = new_node;
+        new_node->prev = tail;
+        tail = new_node;
         return;
     }
 };
-void deletesameneighbour(node* &head,node* &tail)
+void deletesameneighbour(node *&head, node *&tail)
 {
-    node* currnode=tail->prev; //second last node
-    while(currnode!=head)
+    node *currnode = tail->prev; // second last node
+    while (currnode != head)
     {
-        node*prevnode=currnode->prev;
-        node*nextnode=currnode->next;
-        if(prevnode->val==nextnode->val)
+        node *prevnode = currnode->prev;
+        node *nextnode = currnode->next;
+        if (prevnode->val == nextnode->val)
         {
             // i need to delete the curr node
-            prevnode->next=nextnode;
-            nextnode->prev=prevnode;
+            prevnode->next = nextnode;
+            nextnode->prev = prevnode;
             free(currnode);
-
         }
-currnode=prevnode;
-
+        currnode = prevnode;
     }
-
-
 }
 int main()
 {
     DoublyLinkedList dll;
-    dll.insertAtStart(1);
+    dll.insertAtEnd(2);
+    dll.insertAtEnd(1);
+    dll.insertAtEnd(2);
+    dll.insertAtEnd(1);
     dll.display();
-    dll.insertAtStart(2);
+    deletesameneighbour(dll.head, dll.tail);
     dll.display();
-    dll.insertAtStart(3);
-    dll.display();
-
-    return 0; 
+    return 0;
 }
