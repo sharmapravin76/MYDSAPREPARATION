3c6565f3953c31c058240047e7f586f54688ddfc
pravin <sharmapravin7631@gmail.com>
2024-08-01 23:04:25 +0530

    changes

--- collegewallah/Dynamicprogramming/part3/15PartitionEqualSubsetSumbottomup.cpp.cpp
+++ collegewallah/Dynamicprogramming/part3/15PartitionEqualSubsetSumbottomup.cpp.cpp
@@ -120,37 +120,37 @@ public:
 
 // Updates for j from 1 to 11:
 
-// j = 1: nums[3] = 5 > 1: 
+// j = 1: nums[3] = 5 > 1:
 // dp[3][1] = dp[4][1] = false
 // dp[3][1] = dp[4][1] = false
-// j = 2: nums[3] = 5 > 2: 
+// j = 2: nums[3] = 5 > 2:
 // dp[3][2] = dp[4][2] = false
 // dp[3][2] = dp[4][2] = false
-// j = 3: nums[3] = 5 > 3: 
+// j = 3: nums[3] = 5 > 3:
 // dp[3][3] = dp[4][3] = false
 // dp[3][3] = dp[4][3] = false
-// j = 4: nums[3] = 5 > 4: 
+// j = 4: nums[3] = 5 > 4:
 // dp[3][4] = dp[4][4] = false
 // dp[3][4] = dp[4][4] = false
-// j = 5: nums[3] = 5 <= 5: 
+// j = 5: nums[3] = 5 <= 5:
 // dp[3][5] = dp[4][0] || dp[4][5] = true || false = true
 // dp[3][5] = dp[4][0] || dp[4][5] = true || false = true
-// j = 6: nums[3] = 5 <= 6: 
+// j = 6: nums[3] = 5 <= 6:
 // dp[3][6] = dp[4][1] || dp[4][6] = false || false = false
 // dp[3][6] = dp[4][1] || dp[4][6] = false || false = false
-// j = 7: nums[3] = 5 <= 7: 
+// j = 7: nums[3] = 5 <= 7:
 // dp[3][7] = dp[4][2] || dp[4][7] = false || false = false
 // dp[3][7] = dp[4][2] || dp[4][7] = false || false = false
-// j = 8: nums[3] = 5 <= 8: 
+// j = 8: nums[3] = 5 <= 8:
 // dp[3][8] = dp[4][3] || dp[4][8] = false || false = false
 // dp[3][8] = dp[4][3] || dp[4][8] = false || false = false
-// j = 9: nums[3] = 5 <= 9: 
+// j = 9: nums[3] = 5 <= 9:
 // dp[3][9] = dp[4][4] || dp[4][9] = false || false = false
 // dp[3][9] = dp[4][4] || dp[4][9] = false || false = false
-// j = 10: nums[3] = 5 <= 10: 
+// j = 10: nums[3] = 5 <= 10:
 // dp[3][10] = dp[4][5] || dp[4][10] = false || false = false
 // dp[3][10] = dp[4][5] || dp[4][10] = false || false = false
-// j = 11: nums[3] = 5 <= 11: 
+// j = 11: nums[3] = 5 <= 11:
 // dp[3][11] = dp[4][6] || dp[4][11] = false || false = false
 // dp[3][11] = dp[4][6] || dp[4][11] = false || false = false
 // After this iteration, the DP table is updated as follows:
@@ -175,37 +175,37 @@ public:
 // dp[2][j] = dp[3][j]
 // Updates for j from 1 to 11:
 
-// j = 1: nums[2] = 11 > 1: 
+// j = 1: nums[2] = 11 > 1:
 // dp[2][1] = dp[3][1] = false
 // dp[2][1] = dp[3][1] = false
-// j = 2: nums[2] = 11 > 2: 
+// j = 2: nums[2] = 11 > 2:
 // dp[2][2] = dp[3][2] = false
 // dp[2][2] = dp[3][2] = false
-// j = 3: nums[2] = 11 > 3: 
+// j = 3: nums[2] = 11 > 3:
 // dp[2][3] = dp[3][3] = false
 // dp[2][3] = dp[3][3] = false
-// j = 4: nums[2] = 11 > 4: 
+// j = 4: nums[2] = 11 > 4:
 // dp[2][4] = dp[3][4] = false
 // dp[2][4] = dp[3][4] = false
-// j = 5: nums[2] = 11 > 5: 
+// j = 5: nums[2] = 11 > 5:
 // dp[2][5] = dp[3][5] = true
 // dp[2][5] = dp[3][5] = true
-// j = 6: nums[2] = 11 > 6: 
+// j = 6: nums[2] = 11 > 6:
 // dp[2][6] = dp[3][6] = false
 // dp[2][6] = dp[3][6] = false
-// j = 7: nums[2] = 11 > 7: 
+// j = 7: nums[2] = 11 > 7:
 // dp[2][7] = dp[3][7] = false
 // dp[2][7] = dp[3][7] = false
-// j = 8: nums[2] = 11 > 8: 
+// j = 8: nums[2] = 11 > 8:
 // dp[2][8] = dp[3][8] = false
 // dp[2][8] = dp[3][8] = false
-// j = 9: nums[2] = 11 > 9: 
+// j = 9: nums[2] = 11 > 9:
 // dp[2][9] = dp[3][9] = false
 // dp[2][9] = dp[3][9] = false
-// j = 10: nums[2] = 11 > 10: 
+// j = 10: nums[2] = 11 > 10:
 // dp[2][10] = dp[3][10] = false
 // dp[2][10] = dp[3][10] = false
-// j = 11: nums[2] = 11 <= 11: 
+// j = 11: nums[2] = 11 <= 11:
 // dp[2][11] = dp[3][0] || dp[3][11] = true || false = true
 // dp[2][11] = dp[3][0] || dp[3][11] = true || false = true
 // After this iteration, the DP table is updated as follows:
@@ -230,37 +230,37 @@ public:
 // dp[1][j] = dp[2][j]
 // Updates for j from 1 to 11:
 
-// j = 1: nums[1] = 5 > 1: 
+// j = 1: nums[1] = 5 > 1:
 // dp[1][1] = dp[2][1] = false
 // dp[1][1] = dp[2][1] = false
-// j = 2: nums[1] = 5 > 2: 
+// j = 2: nums[1] = 5 > 2:
 // dp[1][2] = dp[2][2] = false
 // dp[1][2] = dp[2][2] = false
-// j = 3: nums[1] = 5 > 3: 
+// j = 3: nums[1] = 5 > 3:
 // dp[1][3] = dp[2][3] = false
 // dp[1][3] = dp[2][3] = false
-// j = 4: nums[1] = 5 > 4: 
+// j = 4: nums[1] = 5 > 4:
 // dp[1][4] = dp[2][4] = false
 // dp[1][4] = dp[2][4] = false
-// j = 5: nums[1] = 5 <= 5: 
+// j = 5: nums[1] = 5 <= 5:
 // dp[1][5] = dp[2][0] || dp[2][5] = true || true = true
 // dp[1][5] = dp[2][0] || dp[2][5] = true || true = true
-// j = 6: nums[1] = 5 <= 6: 
+// j = 6: nums[1] = 5 <= 6:
 // dp[1][6] = dp[2][1] || dp[2][6] = false || false = false
 // dp[1][6] = dp[2][1] || dp[2][6] = false || false = false
-// j = 7: nums[1] = 5 <= 7: 
+// j = 7: nums[1] = 5 <= 7:
 // dp[1][7] = dp[2][2] || dp[2][7] = false || false = false
 // dp[1][7] = dp[2][2] || dp[2][7] = false || false = false
-// j = 8: nums[1] = 5 <= 8: 
+// j = 8: nums[1] = 5 <= 8:
 // dp[1][8] = dp[2][3] || dp[2][8] = false || false = false
 // dp[1][8] = dp[2][3] || dp[2][8] = false || false = false
-// j = 9: nums[1] = 5 <= 9: 
+// j = 9: nums[1] = 5 <= 9:
 // dp[1][9] = dp[2][4] || dp[2][9] = false || false = false
 // dp[1][9] = dp[2][4] || dp[2][9] = false || false = false
-// j = 10: nums[1] = 5 <= 10: 
+// j = 10: nums[1] = 5 <= 10:
 // dp[1][10] = dp[2][5] || dp[2][10] = true || false = true
 // dp[1][10] = dp[2][5] || dp[2][10] = true || false = true
-// j = 11: nums[1] = 5 <= 11: 
+// j = 11: nums[1] = 5 <= 11:
 // dp[1][11] = dp[2][6] || dp[2][11] = false || true = true
 // dp[1][11] = dp[2][6] || dp[2][11] = false || true = true
 // After this iteration, the DP table is updated as follows:
@@ -285,37 +285,37 @@ public:
 // dp[0][j] = dp[1][j]
 // Updates for j from 1 to 11:
 
-// j = 1: nums[0] = 1 <= 1: 
+// j = 1: nums[0] = 1 <= 1:
 // dp[0][1] = dp[1][0] || dp[1][1] = true || false = true
 // dp[0][1] = dp[1][0] || dp[1][1] = true || false = true
-// j = 2: nums[0] = 1 <= 2: 
+// j = 2: nums[0] = 1 <= 2:
 // dp[0][2] = dp[1][1] || dp[1][2] = false || false = false
 // dp[0][2] = dp[1][1] || dp[1][2] = false || false = false
-// j = 3: nums[0] = 1 <= 3: 
+// j = 3: nums[0] = 1 <= 3:
 // dp[0][3] = dp[1][2] || dp[1][3] = false || false = false
 // dp[0][3] = dp[1][2] || dp[1][3] = false || false = false
-// j = 4: nums[0] = 1 <= 4: 
+// j = 4: nums[0] = 1 <= 4:
 // dp[0][4] = dp[1][3] || dp[1][4] = false || false = false
 // dp[0][4] = dp[1][3] || dp[1][4] = false || false = false
-// j = 5: nums[0] = 1 <= 5: 
+// j = 5: nums[0] = 1 <= 5:
 // dp[0][5] = dp[1][4] || dp[1][5] = false || true = true
 // dp[0][5] = dp[1][4] || dp[1][5] = false || true = true
-// j = 6: nums[0] = 1 <= 6: 
+// j = 6: nums[0] = 1 <= 6:
 // dp[0][6] = dp[1][5] || dp[1][6] = true || false = true
 // dp[0][6] = dp[1][5] || dp[1][6] = true || false = true
-// j = 7: nums[0] = 1 <= 7: 
+// j = 7: nums[0] = 1 <= 7:
 // dp[0][7] = dp[1][6] || dp[1][7] = false || false = false
 // dp[0][7] = dp[1][6] || dp[1][7] = false || false = false
-// j = 8: nums[0] = 1 <= 8: 
+// j = 8: nums[0] = 1 <= 8:
 // dp[0][8] = dp[1][7] || dp[1][8] = false || false = false
 // dp[0][8] = dp[1][7] || dp[1][8] = false || false = false
-// j = 9: nums[0] = 1 <= 9: 
+// j = 9: nums[0] = 1 <= 9:
 // dp[0][9] = dp[1][8] || dp[1][9] = false || false = false
 // dp[0][9] = dp[1][8] || dp[1][9] = false || false = false
-// j = 10: nums[0] = 1 <= 10: 
+// j = 10: nums[0] = 1 <= 10:
 // dp[0][10] = dp[1][9] || dp[1][10] = false || true = true
 // dp[0][10] = dp[1][9] || dp[1][10] = false || true = true
-// j = 11: nums[0] = 1 <= 11: 
+// j = 11: nums[0] = 1 <= 11:
 // dp[0][11] = dp[1][10] || dp[1][11] = true || true = true
 // dp[0][11] = dp[1][10] || dp[1][11] = true || true = true
 // After this iteration, the final DP table is:
@@ -324,7 +324,7 @@ public:
 // 0	true	true	false	false	false	true	true	false	false	false	true	true
 // 1	true	false	false	false	false	true	false	false	false	false	true	true
 // 2	true	false	false	false	false	true	false	false	false	false	false	true
-// 3	true	false	false	false	false	true	false	false				
+// 3	true	false	false	false	false	true	false	false
 
 // nums[3] <= j but here what is the value of j
 // ChatGPT
@@ -376,9 +376,3 @@ public:
 // j > 5: The table remains false.
 // j < 5: The table remains false.
 // This approach helps us ensure that nums[3] (5) can only contribute to sums greater than or equal to its value.
-
-
-
-
-
-
