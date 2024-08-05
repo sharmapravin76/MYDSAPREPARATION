0637b28ec71100cdd06333511896fe3348a00734
pravin <sharmapravin7631@gmail.com>
2024-08-06 00:28:02 +0530

    changes

--- collegewallah/Graph/part23/2allpath.cpp
+++ collegewallah/Graph/part23/2allpath.cpp
@@ -3,13 +3,14 @@
 #include <unordered_set>
 #include <vector>
 using namespace std;
-int v;
 vector<list<int>> graph;
 unordered_set<int> visited;
 vector<vector<int>> result;
+int v;
 void add_edge(int src, int dest, bool bi_dir = true)
 {
     graph[src].push_back(dest);
+
     if (bi_dir)
     {
         graph[dest].push_back(src);
@@ -18,10 +19,12 @@ void add_edge(int src, int dest, bool bi_dir = true)
 void dfs(int curr, int end, vector<int> &path)
 {
     if (curr == end)
+    {
         path.push_back(curr);
-    result.push_back(path);
-    path.pop_back();
-    return;
+        result.push_back(path);
+        path.pop_back();
+        return;
+    }
 
     visited.insert(curr);
     path.push_back(curr);
@@ -29,43 +32,61 @@ void dfs(int curr, int end, vector<int> &path)
     {
         if (not visited.count(neighbour))
         {
-            dfs(neighbour, end, path);
+         dfs(neighbour, end, path);
+        
         }
     }
     path.pop_back();
     visited.erase(curr);
-    return;
+    return ;
 }
 void allpath(int src, int dest)
 {
-    vector<int> v;
-
-    dfs(src, dest, v);
+    vector<int>v;
+    return dfs(src, dest, v);
 }
+
 int main()
 {
-
     cin >> v;
     graph.resize(v, list<int>());
     int e;
     cin >> e;
     visited.clear();
     while (e--)
-
     {
         int s, d;
+
         cin >> s >> d;
         add_edge(s, d);
     }
     int x, y;
-    allpath(x, y);
-    for (auto path : result)
-    {
-        for (auto el : path)
+    cin >> x >> y;
+     allpath(x, y);
+     for(auto path:result)
+     {
+        for(auto el:path)
         {
-            cout << el << " . ";
-        }
-        cout << "\n";
-    }
+        cout<<el<<" ";
+     }
+     cout<<"\n";
+     }
     return 0;
 }
+
+// 7
+// 8
+// 0 1
+// 0 4
+// 1 3
+// 4 3
+// 1 5
+// 5 6
+// 6 2
+// 5 2
+// -->this input is for path from 0 to 6 ---?0 6
+//output
+// 0 1 5 6       
+// 0 1 5 2 6     
+// 0 4 3 1 5 6   
+// 0 4 3 1 5 2 6 
---
+++ collegewallah/Graph/part23/3shortest_bfs_path.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <limits.h>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+vector<list<int>> graph;
+vector<vector<int>> result;
+unordered_set<int> visited;
+int v;
+void add_edge(int src, int dest, bool bi_dir = true)
+{
+    graph[src].push_back(dest);
+    if (bi_dir)
+    {
+        graph[dest].push_back(src);
+    }
+}
+void bfs(int src, int dest, vector<int> &dist)
+{
+    queue<int> qu;
+    visited.clear();
+    dist.resize(v, INT_MAX);
+    dist[src] = 0;
+    visited.insert(src);
+    qu.push(src);
+    while (not qu.empty())
+    {
+        int curr = qu.front();
+        cout<<curr<<" ";
+        qu.pop();
+        for (auto neighbour : graph[curr])
+        {
+            if (not visited.count(neighbour))
+            {
+                qu.push(neighbour);
+                visited.insert(neighbour);
+                dist[neighbour] = dist[curr] + 1;
+            }
+        }
+    }
+    cout<<"\n";
+}
+int main()
+{
+    cin >> v;
+    graph.resize(v, list<int>());
+    int e;
+    cin >> e;
+    visited.clear();
+    while (e--)
+    {
+        int s, d;
+        cin >> s >> d;
+        add_edge(s, d);
+    }
+    int x, y;
+    cin >> x >> y;
+    vector<int>dist;
+    bfs(x,y,dist);
+for(int i=0;i<dist.size();i++)
+{
+    cout<<dist[i]<<" ";
+}
+    return 0;
+}
+
+// input
+// 7
+// 8  
+// 0 1
+// 0 4
+// 1 3
+// 1 5
+// 4 3
+// 5 6
+// 6 2
+// 5 2
+// 0 6 this is the path from 0 to 6
+
+// output
+// 0 1 4 3 5 6 2 this the path way of the distance
+// 0 1 3 2 1 2 3 this is the shortest path
